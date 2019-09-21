369da076c5e9343f8e610bedaed069bcf46904a4
GodGamer029 <godgamer029@gmail.com>
2019-09-21 15:00:52 +0200

    Clean UP & Upload new files

--- src/main/java/yangbot/YangBot.java
+++ src/main/java/yangbot/YangBot.java
@@ -2,10 +2,7 @@ package yangbot;
 
 import rlbot.Bot;
 import rlbot.ControllerState;
-import rlbot.cppinterop.RLBotDll;
-import rlbot.flat.BallPrediction;
 import rlbot.flat.GameTickPacket;
-import rlbot.flat.PredictionSlice;
 import yangbot.input.BallData;
 import yangbot.input.CarData;
 import yangbot.input.DataPacket;
@@ -16,9 +13,6 @@ import yangbot.manuever.AerialManuver;
 import yangbot.manuever.LowGravKickoffManuver;
 import yangbot.util.AdvancedRenderer;
 import yangbot.util.ControlsOutput;
-import yangbot.util.MathUtils;
-import yangbot.vector.Matrix3x3;
-import yangbot.vector.Vector3;
 
 import java.awt.*;
 
@@ -87,130 +81,12 @@ public class YangBot implements Bot {
                 if(input.gameInfo.isKickoffPause())
                     nextState = State.RESET;
 
-                Vector3 targetGoal = new Vector3(0, -Math.signum(car.team - 0.5f) * 2900, 400);
-                Vector3 ownGoal = new Vector3(0, Math.signum(car.team - 0.5f) * 2900, 400);
-                float carToGoalDist = (float) car.position.add(car.velocity.mul(0.2f)).distance(targetGoal);
-                float ballToGoalDist = (float) ball.position.add(ball.velocity.mul(0.2f)).distance(targetGoal);
-                aerialManuver = new AerialManuver();
-
-                headingOut = false;
-                // Heading out
-                if(carToGoalDist + 200 < ballToGoalDist && ball.velocity.magnitude() > 700 && Math.abs(car.position.y - ownGoal.y) > 800){
-                    headingOut = true;
-                    System.out.println("Aight ima head out");
-                    float closest = 10000000;
-                    Vector3 closeer = null;
-                    for(float tim = 0; tim < 8; tim += 1/15f){
-                        aerialManuver.arrivalTime = tim + car.elapsedSeconds;
-                        //Math.signum(car.team - 0.5f) *
-                        aerialManuver.target = new Vector3(0, MathUtils.clip(ball.position.y + (ball.position.y - car.position.y) + ball.velocity.y * 0.5f, -3000, 3000), 650);
-                        aerialManuver.target_orientation = Matrix3x3.lookAt(new Vector3(0, 0, -1), new Vector3(0, Math.signum(-aerialManuver.target.y), 0));
-                        CarData simulation = aerialManuver.simulate(car);
-                        float dist = (float)simulation.position.distance(aerialManuver.target);
-                        if(dist < closest){
-                            closest = dist;
-                            closeer = simulation.position;
-                        }
-                        if(dist <= 50){
-                            aerialManuver.arrivalTime += 0.3f;
-                            nextState = State.RUN;
-                            break;
-                        }
-                    }
-                    if(closeer != null){
-                        renderer.drawCentered3dCube(Color.red, closeer, 500);
-                        renderer.drawCentered3dCube(Color.yellow, aerialManuver.target, 100);
-                    }
-                }else{
-                    // Not heading out
-                    try{
-                        BallPrediction prediction = RLBotDll.getBallPrediction();
-                        float closest = 1000000;
-                        Vector3 closeer = null;
-                        for(int i = 5; i < prediction.slicesLength(); i += 3){
-                            PredictionSlice slice = prediction.slices(i);
-
-                            Vector3 pos = new Vector3(slice.physics().location());
-                            aerialManuver.arrivalTime = slice.gameSeconds();
-                            aerialManuver.target = pos.add(targetGoal.sub(pos).normalized().mul(-85f));
-                            aerialManuver.target_orientation = Matrix3x3.lookAt(car.position.sub(pos).normalized(), new Vector3(0, 0, 1));
-
-                            CarData simulation = aerialManuver.simulate(car);
-                            float dist = (float)simulation.position.distance(aerialManuver.target);
-                            if(dist < closest){
-                                closest = dist;
-                                closeer = simulation.position;
-                            }
-                            if(dist <= 50){
-                                nextState = State.RUN;
-                                break;
-                            }
-                        }
-
-                        if(closeer != null){
-                            renderer.drawCentered3dCube(Color.red, closeer, 500);
-                            renderer.drawCentered3dCube(Color.yellow, aerialManuver.target, 100);
-                        }
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                }
-
                 break;
             }
             case RUN:
-                //aerialManuver.target_orientation = Matrix3x3.lookAt(aerialManuver.target, new Vector3(0, 0, 1));
             {
-                CarData simulation = aerialManuver.simulate(car);
-                float dist = (float)simulation.position.distance(aerialManuver.target);
-                if(dist >= 100){
-                    nextState = State.INIT;
-                    break;
-                }
-            }
-
-
-                if(!headingOut){
-                    try{
-                        Vector3 targetGoal = new Vector3(0, -Math.signum(car.team - 0.5f) * 2900, 400);
-                        BallPrediction prediction = RLBotDll.getBallPrediction();
-                        boolean foundIt = false;
-                        for(int i = 0; i < prediction.slicesLength(); i ++) {
-                            Vector3 pos = new Vector3(prediction.slices(i).physics().location());
-                            pos = pos.add(targetGoal.sub(pos).normalized().mul(-85f));
-                            float dist = (float) aerialManuver.target.distance(pos);
-                            if(dist <= 30 && Math.abs(prediction.slices(i).gameSeconds() - aerialManuver.arrivalTime) <= 0.3f){
-                                if(dist <= 20 && Math.abs(prediction.slices(i).gameSeconds() - aerialManuver.arrivalTime) <= 0.15f ){
-                                   // aerialManuver.target = pos;
-                                    //aerialManuver.arrivalTime = prediction.slices(i).gameSeconds();
-                                }
-                                foundIt = true;
-                                break;
-                            }
-                        }
-                        if(!foundIt)
-                            nextState = State.INIT;
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                }else{
-                    Vector3 ownGoal = new Vector3(0, Math.signum(car.team - 0.5f) * 2900, 400);
-
-                    if(Math.abs(car.position.y - ownGoal.y) < 800)
-                        nextState = State.INIT;
-                }
-                aerialManuver.step(dt, output);
-
-                if(aerialManuver.isDone() || aerialManuver.arrivalTime <= car.elapsedSeconds || (car.hasWheelContact && !output.holdJump())){
-                    nextState = State.INIT;
-                    break;
-                }
-
-                renderer.drawCentered3dCube(Color.green, aerialManuver.target, 50);
-                renderer.drawString2d(String.format("Time left: %.1f", aerialManuver.arrivalTime - car.elapsedSeconds), Color.white, new Point(10, 560), 1, 1);
-                renderer.drawString2d(String.format("Heading Out: %s", headingOut ? "true" : "false"), Color.white, new Point(10, 580), 1, 1);
-
                 break;
+            }
         }
 
         state = nextState;
